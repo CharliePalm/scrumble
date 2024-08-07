36f28106e70573399d3c349d4c186362b195300e
Charlie Palm <charles.palm@tasconline.com>
2024-08-07 13:30:38 -0500

    scrumble:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { ANSWER, Track, hint } from './app.model';
+import { ANSWER, NOISE_AMOUNT, NUM_TRIES, SCRUMBLE_DAY_ONE, Track, generateNoise, hint } from './app.model';
+import * as moment from 'moment';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,38 +9,95 @@ import { ANSWER, Track, hint } from './app.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  showWelcomeModal = true;
+  NUM_TRIES = NUM_TRIES;
   hint = hint;
   title = 'Scrumble';
   track = new Track();
-  blocks = ANSWER.split(' ').map((v) => {
-    const x: number[] = [];
-    for (let i = 0; i < v.length; i ++) {
-      x.push(v.charCodeAt(i));
-    }
-    return x;
-  });
+  moves: ('up' | 'left' | 'down' | 'right' | 'check' | 'x')[] = new Array(NUM_TRIES).fill('-');
+  shakeSubject = new Subject<void>();
+  noise = [generateNoise(), generateNoise()];
+  chars = ANSWER.replace(/\s+/g, '').toUpperCase().split('').map((v) => v.charCodeAt(0))
+  moveIdx = 0;
+  showModal = false;
+  copied = false;
+  numColumns = this.chars.length + 4;
+  solved = false;
+  won = false;
 
-  getChar(blockIdx: number, strIdx: number): string {
-    return this.getStringFromUnicode(this.blocks[blockIdx][strIdx]); // 65 is where capital letters start
+  moveMap = new Map<'up' | 'left' | 'down' | 'right', () => void>([
+    ['up', this.track.up],
+    ['right', this.track.right],
+    ['down', this.track.down],
+    ['left', this.track.left],
+  ]);
+
+  emojiMap = new Map<'up' | 'left' | 'down' | 'right' | 'check' | 'x' | '-', [string, string]>([
+    ['up', ['⬆️', '↑']],
+    ['right', ['➡️', '→']],
+    ['down', ['⬇️', '↓']],
+    ['left', ['⬅️', '←']],
+    ['x', ['❌', 'x']],
+    ['check', ['✅', '✓']],
+    ['-', ['', '—']],
+  ]);
+
+  getLetters(offset = 0): string[] {
+    const baseArray = [...this.noise[0], ...this.chars, ...this.noise[1]];
+    return baseArray.map(
+      (_, idx) => this.getStringFromUnicode(baseArray[Math.abs((idx + this.track.k)) % baseArray.length], offset)
+    ).slice(this.noise[0].length - 2, -1 * (this.noise[1].length - 2));
   }
 
-  getStringFromUnicode(c: number) {
-    return String.fromCharCode((c + (this.track.j)) % 26 + 65);
+  getStringFromUnicode(c: number, offset = 0) {
+    return String.fromCharCode((c + this.track.j + offset) % 26 + 65);
+  }
+
+  getMoveDisplay(): string[] {
+    return this.moves.map((m) => this.emojiMap.get(m)![1]);
   }
 
   check() {
+    if (this.isCorrect()) {
+      this.moves[this.moveIdx++] = 'check';
+      this.solved = true;
+      this.openModal();
+    } else {
+      this.moves[this.moveIdx++] = 'x';
+      this.shakeSubject.next();
+    }
   }
 
   isCorrect(): boolean {
-    const s = ANSWER.replace(' ', '');
-    let sdx = 0;
-    for (let i = 0; i < this.blocks.length; i++) {
-      for (let j = 0; j < this.blocks[i].length; j++) {
-        if (this.getStringFromUnicode(this.blocks[i][j]) != s[sdx]) {
-          return false;
-        }
-      }
-    }
-    return true;
+    return (this.track.j - 13) % 26 === 0 && this.track.k % (ANSWER.length - 1 + NOISE_AMOUNT * 2) === 0;
   }
+
+  getCopyContent(): string {
+    const start = 'Scrumble #' + moment().diff(moment(SCRUMBLE_DAY_ONE), 'day') + '\n' + '"' + hint + '"' + '\n';
+    return this.moves.reduce((prev, curr, idx) => prev + this.emojiMap.get(curr)![0] + ((idx + 1) % 4 === 0 ? '\n' : ''), start).replace('-', '');
+  }
+
+  saveMove(dir: 'up' | 'left' | 'down' | 'right'): void {
+    this.moves[this.moveIdx++] = (dir);
+  }
+
+  makeMove(dir: 'up' | 'left' | 'down' | 'right') {
+    this.moveMap.get(dir)!();
+    this.saveMove(dir);
+    if (this.moveIdx === 12) {
+      this.openModal();
+    }
+  }
+
+  openModal() {
+    this.showModal = true;
+    this.copied = false;
+    this.won = this.moveIdx !== 12;
+  }
+
+  closeWelcomeModal() { this.showWelcomeModal = false; }
+
+  // quick methods - easy black boxes
+  closeModal() { this.showModal = false; this.copied = false; }
+  toggleModal() { this.showModal = !this.showModal; }
 }
--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -1,18 +1,31 @@
-export const ANSWER = "IN TO THE FIRE";
+export const ANSWER = "INTO THE FIRE";
 export const hint = "Out of the frying pan";
+export const SCRUMBLE_DAY_ONE = '2024-07-29T05:00:00.000Z';
+export const NOISE_AMOUNT = 3
+export const DISTANCE = 4
+export const NUM_TRIES = 16;
 export class Track {
   j: number;
   k: number;
   constructor() {
-    this.j = Math.round((Math.random() - .5) * 2 * 14);
-    if ([0, 1, -1].includes(this.j)) this.j += (Math.random() === 0 ? -2 : 2);
-    this.k = Math.round((Math.random() - .5) * 2 * 14);
-    if ([0, 1, -1].includes(this.k)) this.k += (Math.random() === 0 ? -2 : 2);
+    if (Math.random() < .5) {
+      this.j = DISTANCE * -1 - 13;
+    } else {
+      this.j = DISTANCE + 13;
+    }
+    if (Math.random() < .5) {
+      this.k = DISTANCE * -1 - 1;
+    } else {
+      this.k = DISTANCE + 1;
+    }
   }
 
-  right = () => this.j += 1;
-  left = () => this.j -= 1;
-  up = () => this.k += 1;
-  down = () => this.k -= 1;
+  right = () => this.k -= 1;
+  left = () => this.k += 1;
+  up = () => this.j += 1;
+  down = () => this.j -= 1;
 }
 
+export function generateNoise(): number[] {
+  return new Array(NOISE_AMOUNT).fill(65).map((v) => Math.round((Math.random() * 25) + 65));
+}
